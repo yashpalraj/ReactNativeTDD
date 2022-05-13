9cfeda9fc18288ebbec000c8c46fdf88910771e6
Yashpal <YASHPALRAJ@GMAIL.COM>
2022-05-13 15:34:46 +0530

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,13 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  parser: 'babel-eslint',
+  plugins: ['detox', 'jest'],
+  env: {
+    'detox/detox': true,
+    'jest/globals': true,
+  },
+  rules: {
+    'react/prop-types': 'off',
+  },
 };
